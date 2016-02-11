882a653b9c9f6db039a45a41a5720470b9682cc4
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2016-02-11 11:12:35 -0500

    Guessing Game Template

---
+++ GuessingGame/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(39, 8);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(207, 37);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Guessing Game";
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.ForeColor = System.Drawing.Color.White;
+            this.promptLabel.Location = new System.Drawing.Point(12, 78);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(257, 21);
+            this.promptLabel.TabIndex = 1;
+            this.promptLabel.Text = "Enter a Number Between 1 and 100";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(122, 121);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(41, 29);
+            this.inputBox.TabIndex = 2;
+            this.inputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(107, 164);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(73, 32);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(16, 210);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(253, 43);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ GuessingGame/Form1.cs
@@ -0,0 +1,51 @@
+﻿/// Description: The program generates a random number between 1-100 which
+///              the use must guess
+/// Author:      Mr. T
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        //creates a random number between 1 and 100 and stores it in a global 
+        //variable that can be used throughout the program 
+        public static Random randNum = new Random();
+        int rand = randNum.Next(1, 101); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            // get the users guess and place into a variable
+            int guess = Convert.ToInt16(inputBox.Text);
+
+            // check guess against the random value and output appropriate message
+            if (guess < rand)
+            {
+                outputLabel.Text = "Too Low!";
+            }
+            else if (guess > rand)
+            {
+                outputLabel.Text = "Too High!";
+            }
+            else
+            {
+                outputLabel.Text = "You Got it!";
+            }
+
+            // clear out the input box 
+            inputBox.Text = "";
+        }
+    }
+}
